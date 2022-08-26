6822cc22d49c9c18d37a713d816779c7bbfe974f
Hugo Gibbs <hugo.d.gibbs@gmail.com>
2022-08-26 19:15:54 -0400

    .

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,15 @@
-const repeatString = function() {
+const repeatString = function(origin, num) {
+
+    let string = origin;
+    
+    if ( num == 0) return '';
+    if ( num < 0) return 'ERROR';
+    for (let i = 1 ; i < num ; i++){
+        
+        string = origin + string
+    }
+
+    return string;
 
 };
 
