b15d75bc0789811574a11d7c51787c43fe58a5a8
Hugo Gibbs <hugo.d.gibbs@gmail.com>
2022-08-27 17:26:26 -0400

    .

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,26 @@
-const ftoc = function() {
+const ftoc = function(fahrenheit) {
+  celsius = (fahrenheit - 32) * (5/9)
+  
+  if (celsius == 0){
+  return celsius;
+  }
+  else {
+    celsius = parseFloat(celsius.toFixed(1));
+    return celsius;
+  }
 
 };
 
-const ctof = function() {
+const ctof = function(celsius) {
+  fahrenheit = (celsius * (9/5)) + 32
 
+  if (fahrenheit == 0){
+    return fahrenheit
+  }
+  else {
+    fahrenheit = parseFloat(fahrenheit.toFixed(1));
+    return fahrenheit;
+  }
 };
 
 // Do not edit below this line
