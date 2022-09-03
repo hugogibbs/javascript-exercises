8cf89e8c40ee91e27838cc5fc74a0f5f1c8296fa
Hugo Gibbs <hugo.d.gibbs@gmail.com>
2022-09-03 11:26:32 -0400

    .

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function(string) {
+    newString = string.toLowerCase().replace(/[^a-z]/g, "");
+    return (
+        newString
+            .split("")
+            .reverse()
+            .join("") == newString
+);
 };
 
 // Do not edit below this line
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,25 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
 
-};
+let a = 0, b = 1, c = n;
+
+if (n < 0 ) {
+    return 'OOPS';
+}
+
+if (n == 1) {
+    return 1;
+}
+
+for (let i = 2; i <= n; i++){
+    
+    c = a + b;
+    a = b;
+    b = c;
+}
+
+return c;
+
+}
 
 // Do not edit below this line
 module.exports = fibonacci;
